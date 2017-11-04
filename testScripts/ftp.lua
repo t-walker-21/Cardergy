b0319d7a247ed4e7f879ee3980922f585edfba42
t-walker-21 <tjw0018@uah.edu>
2017-11-03 21:53:30 -0500

    implemented lua FTP file upload to server

--- testScripts/ftp.lua
+++ testScripts/ftp.lua
@@ -18,6 +18,19 @@ file:close() --close file
 
 media.playVideo("downloadedFile.mov",system.DocumentsDirectory,true) -- play video from documents directory
 
+--reading from file and sending to FTP server
+
+local path = system.pathForFile("file.txt",system.DocumentsDirectory) --get system (lua) Documents directory
+
+file, errorString = io.open(path,"r") -- open file for reading with path
+
+local contents = file:read("*a") -- read contents of file into contents
+
+f,e = ftp.put("ftp://username:".. password .."@host/directoryName/someFile.txt;type=i",contents) --login to ftp server and upload file at given directory using binary mode (not ascii)
+
+file:close() --close file pointer
+
+
 
 
 
