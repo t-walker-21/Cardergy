db570f0944030300e0e148c047516e546ef9b724
t-walker-21 <tjw0018@uah.edu>
2017-11-07 19:35:09 -0600

    documentation

--- testScripts/videoPhotoCapture.lua
+++ testScripts/videoPhotoCapture.lua
@@ -16,13 +16,18 @@ end
 local function onComplete( event )
     if event.completed then
         media.playVideo( event.url, media.RemoteSource, true, onVideoComplete )
-        print( event.duration )
+	print( event.duration )
         print( event.fileSize )
     end
 end
+
+--media.capturePhoto() for photos https://docs.coronalabs.com/api/library/media/capturePhoto.html
  
 if media.hasSource( media.Camera ) then
     media.captureVideo( { listener=onComplete,preferredQuality="high",preferredMaxDuration=5} )
+
+
+
 else
     native.showAlert( "Corona", "This device does not have a camera.", { "OK" } )
 end
