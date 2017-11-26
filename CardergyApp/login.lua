3cc136800a9808b63061fb368ad534746f22c6ca
t-walker-21 <tjw0018@uah.edu>
2017-11-26 00:37:47 -0600

    fixed login bug

--- CardergyApp/login.lua
+++ CardergyApp/login.lua
@@ -298,7 +298,10 @@ function scene:hide( event )
       -- Called when the scene is on screen (but is about to go off screen).
       -- Insert code here to "pause" the scene.
       -- Example: stop timers, stop animation, stop audio, etc.
-      loading.isVisible = false
+      if loading ~= nil then 
+      		loading.isVisible = false
+      end
+
    elseif ( phase == "did" ) then
       -- Called immediately after scene goes off screen.
       composer.setVariable("passScene", "")
